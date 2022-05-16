7334d49edc174de2d7da17a27b3aedf8711fd9c7
Cesar A. Pires <cesarp@ciandt.com>
2022-05-16 19:21:55 -0300

    Ajuste para validador

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -1,8 +1,8 @@
 public class PrintStringConcat {
-
     public static void main(String[] args){
         String texto1 = "Holberton";
         String texto2 = "School";
-        System.out.printf("Bem vindo a %s %s!\n", texto1, texto2);
+        System.out.print("Bem vindo a ");
+        System.out.println(texto1 +" "+texto2+"!");
     }
 }
