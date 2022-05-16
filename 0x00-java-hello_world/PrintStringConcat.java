6d93b02c96f22a97af755e6723da52fcbbc055b1
Cesar A. Pires <cesarp@ciandt.com>
2022-05-16 18:59:16 -0300

    Ajuste new line final do programa

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -2,7 +2,6 @@ public class PrintStringConcat {
     public static void main(String[] args){
         String texto1 = "Holberton";
         String texto2 = "School";
-        System.out.printf("Bem vindo a ");
-        System.out.printf("%s %s!\n", texto1, texto2);
+        System.out.printf("Bem vindo a %s %s!\n", texto1, texto2);
     }
 }
