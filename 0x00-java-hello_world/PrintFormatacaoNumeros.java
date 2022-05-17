829f9621a0c111bcca4037c38306f5a4900f412c
Cesar A. Pires <cesarp@ciandt.com>
2022-05-17 08:27:35 -0300

    Ajuste aprovação final

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -3,15 +3,13 @@ import java.util.Locale;
 
 public class PrintFormatacaoNumeros {
     public static void main(String[] args){
-        final var localeBrasil = new Locale("pt ", "BR");
         final var localeEUA = new Locale("en ", "EU");
-        NumberFormat moneyFormat = NumberFormat.getCurrencyInstance(localeBrasil);
         NumberFormat decimalFormat = NumberFormat.getNumberInstance(localeEUA);
         decimalFormat.setMaximumFractionDigits(2);
         float taxa = 0.2456f;
         float valor = 7654.321f;
 
-        System.out.printf("Valor: %s\n", moneyFormat.format(valor));
+        System.out.printf("Valor: R$ %s \n", decimalFormat.format(valor));
         System.out.println("Taxa: "+ decimalFormat.format(taxa)+"%");
     }
 }
