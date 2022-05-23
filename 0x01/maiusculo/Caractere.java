ccfde7b8e5b0084fb77f1953706e24e3f2f6650c
Cesar A. Pires <cesarp@ciandt.com>
2022-05-23 16:36:45 -0300

    Projeto dia 23/05

--- 0x01/maiusculo/Caractere.java
+++ 0x01/maiusculo/Caractere.java
@@ -1,5 +1,83 @@
 public class Caractere {
-    public static EhMaiusculo(char letra) {
-        
+    public static boolean EhMaiusculo(char c) {
+        boolean isMaisculo = true;
+        switch (c){
+            case 'a':
+                isMaisculo = false;
+                break;
+            case 'b':
+                isMaisculo = false;
+                break;
+            case 'c':
+                isMaisculo = false;
+                break;
+            case 'd':
+                isMaisculo = false;
+                break;
+            case 'e':
+                isMaisculo = false;
+                break;
+            case 'f':
+                isMaisculo = false;
+                break;
+            case 'g':
+                isMaisculo = false;
+                break;
+            case 'h':
+                isMaisculo = false;
+                break;
+            case 'i':
+                isMaisculo = false;
+                break;
+            case 'j':
+                isMaisculo = false;
+                break;
+            case 'k':
+                isMaisculo = false;
+                break;
+            case 'l':
+                isMaisculo = false;
+                break;
+            case 'm':
+                isMaisculo = false;
+                break;
+            case 'n':
+                isMaisculo = false;
+                break;
+            case 'o':
+                isMaisculo = false;
+                break;
+            case 'p':
+                isMaisculo = false;
+                break;
+            case 'q':
+                isMaisculo = false;
+                break;
+            case 'r':
+                isMaisculo = false;
+                break;
+            case 's':
+                isMaisculo = false;
+                break;
+            case 'u':
+                isMaisculo = false;
+                break;
+            case 'v':
+                isMaisculo = false;
+                break;
+            case 'x':
+                isMaisculo = false;
+                break;
+            case 'z':
+                isMaisculo = false;
+                break;
+            case 'w':
+                isMaisculo = false;
+                break;
+            case 'y':
+                isMaisculo = false;
+                break;
+        }
+        return isMaisculo;
     }
 }
