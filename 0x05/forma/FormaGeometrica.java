07c445925c72a8defd63f06f930911d2a597dda3
Cesar A. Pires <cesarp@ciandt.com>
2022-05-24 10:30:59 -0300

    Projetos aula 11

---
+++ 0x05/forma/FormaGeometrica.java
@@ -0,0 +1,5 @@
+public class FormaGeometrica {
+    public double area() {
+        java.lang.UnsupportedOperationException("Funcao area nao implementada");
+    }
+}
---
+++ 0x05/quadrado/Quadrado.java
@@ -0,0 +1,21 @@
+public class Quadrado extends Retangulo {
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if (lado < 0) {
+            throw new java.lang.IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        }
+        setAltura(lado);
+        setLargura(lado);
+        this.lado = lado;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Quadrado] "+getLargura());
+    }
+}
