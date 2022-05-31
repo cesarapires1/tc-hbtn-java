9246b8c4e472a536fc7b3209e5b642c78d084478
Cesar A. Pires <cesarp@ciandt.com>
2022-05-31 10:20:47 -0300

    Projeto dia 15 Task 0. Array

--- List/numeros/ManipularArrayNumeros.java
+++ List/numeros/ManipularArrayNumeros.java
@@ -2,22 +2,21 @@ import java.util.*;
 
 public class ManipularArrayNumeros {
 
-    static public void adicionarNumero(List numeros, Integer i) {
+    static public void adicionarNumero(List numeros, Integer i) throws Exception {
         if (buscarPosicaoNumero(numeros, i) == -1) {
-            throw new IllegalArgumentException("Numero jah contido na lista");
+            throw new Exception("Numero jah contido na lista");
         }
         numeros.add(i);
     }
 
-    static public void removerNumero(List numeros, Integer i){
+    static public void removerNumero(List numeros, Integer i) throws Exception {
         if (buscarPosicaoNumero(numeros, i) != -1) {
-            throw new IllegalArgumentException("Numero nao encontrado na lista");
+            throw new Exception("Numero nao encontrado na lista");
         }
         numeros.remove(i);
     }
 
     static public void substituirNumero(List numeros, Integer numeroSubstituir, Integer numeroSubstituto){
-//        Integer numero = numeroSubstituir*numeroSubstituto;
         if (buscarPosicaoNumero(numeros, numeroSubstituir) == -1) {
             numeros.set(numeros.indexOf(numeroSubstituir), numeroSubstituto);
         } else {
