cae9aa88040be32379bef09377e41a7c55e2b61c
Cesar A. Pires <cesarp@ciandt.com>
2022-06-06 14:16:54 -0300

    Projeto dia 20 Task 0. Blog

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Blog {
+    List<Post> postagem = new ArrayList<>();
+
+    public void adicionarPostagem(Post postagem) {
+        this.postagem.add(postagem);
+    }
+
+    public Set<String> obterTodosAutores() {
+        Set<String> autores = new TreeSet<>();
+        for (Post post : postagem) {
+            autores.add(post.getAutor());
+        }
+        return autores;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria() {
+        Map<String, Integer> contagem = new TreeMap<>();
+        for (Post post : postagem) {
+            int contagemAtual = 1;
+            if (contagem.containsKey(post.getCategoria())) {
+                contagemAtual = contagem.get(post.getCategoria());
+                contagemAtual++;
+            }
+            contagem.put(post.getCategoria(), contagemAtual);
+        }
+        return contagem;
+    }
+}
