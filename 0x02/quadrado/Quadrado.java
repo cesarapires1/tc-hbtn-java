68b1603d4dceceeab9adc18e22b1d227304c99c1
Cesar A. Pires <cesarp@ciandt.com>
2022-06-07 15:24:02 -0300

    Projeto dia 21 Task 1. Área Quadrado

---
+++ 0x02/quadrado/Quadrado.java
@@ -0,0 +1,8 @@
+public class Quadrado {
+    public static double area(double lado1) {
+        if (lado1 < 0) {
+            throw new IllegalArgumentException("Lado deve possuir valor positivo");
+        }
+        return lado1*lado1;
+    }
+}
