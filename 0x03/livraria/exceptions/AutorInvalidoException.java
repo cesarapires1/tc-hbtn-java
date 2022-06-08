d6bf0b54f0f1af26905db8063e985290c9c270f4
Cesar A. Pires <cesarp@ciandt.com>
2022-06-08 15:08:37 -0300

    Projeto dia 22 Task 1. Livraria

---
+++ 0x03/livraria/exceptions/AutorInvalidoException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class AutorInvalidoException extends Exception{
+    public AutorInvalidoException(String message) {
+        super(message);
+    }
+
+    public AutorInvalidoException() {
+    }
+}
