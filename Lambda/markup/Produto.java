588ebde78b26e6c3624da26ef5b0011bdf1bab16
Cesar A. Pires <cesarp@ciandt.com>
2022-06-09 16:14:56 -0300

    Projeto dia 23 Task 0. Produto Markup

--- Lambda/markup/Produto.java
+++ Lambda/markup/Produto.java
@@ -6,8 +6,8 @@ public class Produto {
     private double preco;
     private double percentualMarkup = 10;
 
-    Supplier precoComMarkup = () -> this.getPreco()*(1+this.percentualMarkup/100);
-    Consumer atualizarMarkup = novoValor -> setPercentualMarkup((Double) novoValor);
+    Supplier precoComMarkup = () -> (double) this.getPreco()*(1+this.percentualMarkup/100);
+    Consumer atualizarMarkup = (novoValor) -> setPercentualMarkup((Double) novoValor);
 
 
     public Produto(double preco, String nome) {
@@ -20,39 +20,14 @@ public class Produto {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public double getPreco() {
         return preco;
     }
 
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-
-    public double getPercentualMarkup() {
-        return percentualMarkup;
-    }
 
     public void setPercentualMarkup(double percentualMarkup) {
         this.percentualMarkup = percentualMarkup;
     }
 
-    public Supplier getPrecoComMarkup() {
-        return precoComMarkup;
-    }
 
-    public void setPrecoComMarkup(Supplier precoComMarkup) {
-        this.precoComMarkup = precoComMarkup;
-    }
-
-    public Consumer getAtualizarMarkup() {
-        return atualizarMarkup;
-    }
-
-    public void setAtualizarMarkup(Consumer atualizarMarkup) {
-        this.atualizarMarkup = atualizarMarkup;
-    }
 }
