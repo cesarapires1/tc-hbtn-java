5098ed13e8b0237bf17267801f2b839022bfb799
Cesar A. Pires <cesarp@ciandt.com>
2022-06-10 09:36:32 -0300

    Projeto dia 23 Task 1. Salario Funcionario

---
+++ Lambda/liquido/FolhaPagamento.java
@@ -0,0 +1,8 @@
+public class FolhaPagamento {
+
+
+    public static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario calculador){
+        return calculador.calcular(funcionario.getSalario());
+    }
+
+}
