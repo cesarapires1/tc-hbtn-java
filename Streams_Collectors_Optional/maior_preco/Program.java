eec727b9543142c18e4fbe141088c2aca8025baf
Cesar A. Pires <cesarp@ciandt.com>
2022-06-13 12:37:20 -0300

    Projeto dia 25 Task 1. Relatório Produto com maior preço

--- Streams_Collectors_Optional/maior_preco/Program.java
+++ Streams_Collectors_Optional/maior_preco/Program.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
         Produto produto1 = new Produto(1,
                 "Carro Vortex", CategoriaProduto.BRINQUEDO, 799.90);
 
