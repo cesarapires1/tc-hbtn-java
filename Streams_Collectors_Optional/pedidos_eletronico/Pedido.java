fa9e8df2dcd8a4103d7e7d2cd4b66a924248f488
Cesar A. Pires <cesarp@ciandt.com>
2022-06-13 15:58:26 -0300

    Projeto dia 25 Task 3. Relatório Pedidos com Eletrônicos

---
+++ Streams_Collectors_Optional/pedidos_eletronico/Pedido.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Pedido {
+    private int codigo;
+    private List<Produto> produtos = new ArrayList<>();
+    private Cliente cliente;
+
+    public Pedido(int codigo, List<Produto> produtos, Cliente cliente) {
+        this.codigo = codigo;
+        this.produtos = produtos;
+        this.cliente = cliente;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    @Override
+    public String toString() {
+        String string = String.format("[%d] %s ", getCodigo(), getCliente().getNome());
+        return string;
+    }
+}
