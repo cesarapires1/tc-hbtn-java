29919bc5205dacd020077e35dcadc5ba4e3f561f
Cesar A. Pires <cesarp@ciandt.com>
2022-06-14 15:26:06 -0300

    Projeto dia 26 Task 1. Contagem de pessoas por cargo

---
+++ Streams_Collector_Optional_exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> todasPessoas) {
+
+        TreeMap<String, TreeSet<Pessoa>> listaPessoasCargo = todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, () -> new TreeMap<>(Comparator.reverseOrder()), Collectors.toCollection(TreeSet::new)));
+
+        return listaPessoasCargo;
+
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> todasPessoas) {
+
+        Map<String, Long> listaContagemCargo;
+
+        listaContagemCargo = todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+
+        return listaContagemCargo;
+    }
+}
