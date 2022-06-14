1954e775651de784815edf896f094fb1805c7090
Cesar A. Pires <cesarp@ciandt.com>
2022-06-14 15:33:24 -0300

    Projeto dia 26 Task 2. Contagem de pessoas por idade e cargo

---
+++ Streams_Collector_Optional_exercicios/quantidade_pessoas_por_cargo_idade/Pessoa.java
@@ -0,0 +1,49 @@
+import java.util.Comparator;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    @Override
+    public String toString() {
+        String string;
+        string = String.format("[%d] %s %s %d R$ %.6f", getCodigo(), getNome(), getCargo(), getIdade(), getSalario());
+        return string;
+    }
+
+    @Override
+    public int compareTo(Pessoa pessoa) {
+        return getNome().compareTo(pessoa.getNome());
+    }
+}
