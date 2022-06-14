d6c23dc16e6ebcd54f8b3b4154c6222e37216204
Cesar A. Pires <cesarp@ciandt.com>
2022-06-14 08:36:36 -0300

    Projeto dia 25 Task 4. Eletronicos com 15% de desconto

---
+++ Streams_Collectors_Optional/desconto_eletronicos/Consulta.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        List<Produto> pedidoFiltrado;
+        List<Produto> produtosPedido = pedido.getProdutos();
+
+        pedidoFiltrado = produtosPedido
+                .stream()
+                .filter(produto -> produto.getCategoria().equals(CategoriaProduto.LIVRO))
+                .collect(Collectors.toList());
+
+        return pedidoFiltrado;
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        Produto produtoMaioPreco;
+
+        produtoMaioPreco = produtos
+                .stream()
+                .max(Comparator.comparing(produto -> produto.getPreco()))
+                .get();
+
+        return produtoMaioPreco;
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, int precoMinimo) {
+        List<Produto> pedidoFiltrado;
+
+        pedidoFiltrado = produtos
+                .stream()
+                .filter(produto -> produto.getPreco() >= precoMinimo)
+                .toList();
+
+        return pedidoFiltrado;
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        List<Pedido> pedidoFiltrado;
+
+        pedidoFiltrado = pedidos
+                .stream()
+                .filter(pedido -> pedido.getProdutos()
+                        .stream()
+                        .anyMatch(produto -> produto.getCategoria().equals(CategoriaProduto.ELETRONICO)))
+                .toList();
+
+        return pedidoFiltrado;
+    }
+
+    public static List<Produto> aplicar15PorcentoDescontoEletronicos(List<Produto> produtos) {
+        List<Produto> pedidoFiltrado;
+
+        produtos
+                .stream()
+                .filter(produto -> produto.getCategoria().equals(CategoriaProduto.ELETRONICO))
+                .forEach(produto -> {
+                    produto.setPreco(produto.getPreco()*0.85);
+                });
+
+        pedidoFiltrado = produtos
+                .stream()
+                .toList();
+
+        return pedidoFiltrado;
+    }
+}
